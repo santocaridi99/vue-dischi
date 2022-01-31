f8c158bb05ea6ef6e1e72d8395187a30020ec832
santocaridi99 <santocaridi99@gmail.com>
2022-01-31 17:14:19 +0100

    Bonus ,aggiunto loader

--- src/App.vue
+++ src/App.vue
@@ -3,44 +3,49 @@
     <!-- sezione header -->
     <header-box></header-box>
     <!-- sezione main -->
-    <main-container :disks="disks">
+    <main-content :disks="disks">
       <!-- sezione card box -->
       <card-box></card-box>
-    </main-container>
+    </main-content>
   </div>
 </template>
 
 <script>
-// importo componente  Maincontainer 
-import MainContainer from './components/MainContent.vue'
+// importo componente  Maincontainer
+import MainContent from "./components/MainContent.vue";
 // importo axios per effettuare chiamate api
-import axios from 'axios'
+import axios from "axios";
 // importo header
-import HeaderBox from './components/HeaderBox.vue'
+import HeaderBox from "./components/HeaderBox.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    MainContainer,
-    HeaderBox
+    MainContent,
+    HeaderBox,
   },
-  data(){
-    return{
+  data() {
+    return {
       // array di dischi
-      disks:[],
-    }
+      disks: [],
+    };
   },
-  mounted(){
-    // res sta per response 
-    axios .get('https://flynn.boolean.careers/exercises/api/array/music').then((res)=>{
-      // dischi sarà uguale al response del data però dell'array di oggetti chiamato response
-      this.disks = res.data.response
-    });
-  }
-}
+  mounted() {
+    // messo un set timeout così riempie l'array dopo due secondi
+    setTimeout(() => {
+      // res sta per response
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then((res) => {
+          // dischi sarà uguale al response del data però dell'array di oggetti chiamato response
+          this.disks = res.data.response;
+        });
+    },2000);
+  },
+};
 </script>
 
 <style lang="scss">
 // importo cartella main scss
-@import "./style/main.scss"
+@import "./style/main.scss";
 </style>
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,30 +1,34 @@
 <template>
-    <div class="container">
-        <!-- v-for per ogni oggetto disco presente in dischi con chiave l'id unvico presente nel disco-->
-        <card-box v-for="disk in disks" :key="disk.id" :disk="disk"></card-box>
-    </div>
+  <div class="container">
+    <!-- v-for per ogni oggetto disco presente in dischi con chiave l'id unvico presente nel disco-->
+    <card-box v-for="disk in disks" :key="disk.id" :disk="disk"></card-box>
+    <!-- loader content è visibile solo se l'array disk è minore di 10 -->
+    <loader-content v-if="disks.length < 10"></loader-content>
+  </div>
 </template>
 
 <script>
 // importo cardbox
-import CardBox from "./CardBox.vue"
+import CardBox from "./CardBox.vue";
+// import di loader content
+import LoaderContent from "./LoaderContent.vue";
 export default {
-    // esporto i componenti a  cardbox
-    components:{
-        CardBox,
-    },
-    // props  dischi corrisponde ad un array
-    props:{
-        disks:Array,
-    }
-}
+  // esporto i componenti a  cardbox
+  components: {
+    CardBox,
+    LoaderContent,
+  },
+  // props  dischi corrisponde ad un array
+  props: {
+    disks: Array,
+  },
+};
 </script>
 <style lang="scss" scoped>
-.container{
-    width: 1200px;
-    margin: 0 auto;
-    display: flex;
-    flex-wrap: wrap;
-
+.container {
+  width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
 }
 </style>
